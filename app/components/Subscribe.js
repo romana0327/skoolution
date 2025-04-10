04a03dc4e31ca38c2b02a07974aee9eeafe476a8
matboua <matbouaoussama@gmail.com>
2025-04-10 20:05:47 +0100

    Refactor Header component login button styles for improved visual clarity

--- app/components/Subscribe.js
+++ app/components/Subscribe.js
@@ -2,27 +2,28 @@ import { Mail } from "lucide-react";
 
 export default function Subscribe() {
 	return (
-		<section className="bg-skblue text-white w-full flex justify-center px-5 xl:px-0 py-20 text-[14px] lg:text-[16px]">
-			<div className="flex items-center gap-40 text-center sm:text-start w-full lg:w-11/12 xl:w-10/12 max-w-[1260px]">
-				<p className="w-1/2 text-4xl font-semibold leading-[1.2]">
-					Join the world largest online learning platform
+		<section className="bg-skblue text-white w-full flex justify-center px-5 xl:px-0 py-10 text-[14px] lg:text-[16px]">
+			<div className="flex flex-col sm:flex-row items-center text-center gap-5  sm:text-start w-full lg:w-11/12 xl:w-10/12 max-w-[1260px]">
+				<p className="font-semibold text-xl sm:text-2xl lg:text-3xl sm:w-1/2">
+					Join the world largest online{" "}
+					<br className="hidden md:inline-block" /> learning platform
 				</p>
-				<form className="w-1/2 relative">
+				<form className="relative w-full sm:w-1/2">
 					<input
 						type="email"
 						placeholder="name@email.com"
-						className="w-full bg-white placeholder:text-neutral-500 py-8 px-20 outline-0 border-0 text-skblack"
+						className="w-full bg-white placeholder:text-neutral-500 placeholder:pl-6 outline-0 border-0 text-skblack p-5"
 					/>
 					<Mail
 						size={32}
 						strokeWidth={1.25}
-						className="absolute left-10 top-1/2 -translate-y-1/2 text-neutral-500"
+						className="absolute left-2 top-1/2 -translate-y-1/2 text-neutral-500"
 					/>
 					<button
 						type="button"
-						className="absolute right-2 top-1/2 -translate-y-1/2 h-[calc(100%-16px)] bg-skblue py-6 px-12"
+						className="absolute right-2 top-1/2 -translate-y-1/2 h-[calc(100%-16px)] bg-skblue p-2.5"
 					>
-						Rejoignez-nous
+						Rejoignez<span className="inline sm:hidden md:inline">-nous</span>
 					</button>
 				</form>
 			</div>
