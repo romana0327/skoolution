0267ca1b2405aee704ed75f8d4407040bfc94cd2
matboua <matbouaoussama@gmail.com>
2025-04-12 13:45:02 +0100

    Adjust gap spacing in WhySK component for improved layout consistency

--- app/components/WhySK.js
+++ app/components/WhySK.js
@@ -4,7 +4,7 @@ import Button from "./ui/Button";
 export default function WhySK() {
 	return (
 		<section className="bg-skblue-second w-full flex justify-center px-5 xl:px-0 py-16 text-[14px] lg:text-[16px] min-h-screen">
-			<div className="flex flex-col justify-between items-center gap-10 sm:gap-3 md:gap-10 sm:flex-row w-full lg:w-11/12 xl:w-10/12 max-w-[1260px]">
+			<div className="flex flex-col justify-between items-center gap-5 sm:gap-3 md:gap-10 sm:flex-row w-full lg:w-11/12 xl:w-10/12 max-w-[1260px]">
 				{/* Left Column */}
 				<div className="relative flex flex-col items-center w-7/8 min-[460px]:w-2/3 sm:w-full  md:w-2/4 lg:max-w-[350px]">
 					<img src="/SK/student_22.png" alt="Student 2" />
